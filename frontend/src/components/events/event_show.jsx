29bccc5bccd495613f06a28fc6a5ed9ef97097ce
darrickyong <darrickyong@gmail.com>
2020-05-11 14:40:11 -0700

    Add event show page

--- frontend/src/components/events/event_show.jsx
+++ frontend/src/components/events/event_show.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 
-
-class EventIndexItem extends React.Component {
+class EventShow extends React.Component {
   render() {
     const { event } = this.props;
     return (
-      // link to event show page?
       <div>
+        This is Event Show Page
         {event.name}
         {event.date}
       </div>
     )
   }
 }
-
-export default EventIndexItem;
