9b2c8595e4352d2ad368696a365627f891630bd4
tjmccabe <tjmccabe3@gmail.com>
2020-05-12 17:53:52 -0700

    Prep for Heroku Push

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const passport = require('passport');
 const users = require('./routes/api/users');
 const artists = require('./routes/api/artists');
 const events = require('./routes/api/events');
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
