82af5c7efce789d9bac5037471938984e3526fb2
TJ McCabe <tjmccabe3@gmail.com>
2020-05-12 18:04:57 -0700

    Delete keys.js
    these are SECRETS

--- config/keys_dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://dev:H6JUQk4lsSbv11JB@cluster0-790jz.mongodb.net/test?retryWrites=true&w=majority",
-  secretOrKey: "(^`|&*He]wnsSpI_(hGF)RGcgoXcAg",
-  awsAccessKeyId: "AKIAZ7SOQ4MU27VMK7OM",
-  awsSecretAccessKey: "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",
-};
