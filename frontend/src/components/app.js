69b8f4c75b919fdac5b94d09300b245edc7b1ae1
tjmccabe <tjmccabe3@gmail.com>
2020-05-13 00:15:27 -0700

    Filter for live and upcoming events

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -4,7 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import NavBarContainer from './navbar/navbar_container';
 import EventShowContainer from "./events/event_show_container";
 import Modal from './modal/modal';
-import Splash from './splash/splash';
+import SplashContainer from './splash/splash-container';
 import ArtistShowContainer from '../components/artists/artist_show_container'
 
 const App = () => (
@@ -12,7 +12,7 @@ const App = () => (
     <Modal />
     <NavBarContainer />
     <Switch>
-      <Route exact path="/" component={Splash} />
+      <Route exact path="/" component={SplashContainer} />
       <Route path="/artists/:id" component={ArtistShowContainer} />
       <Route path="/events/:id" component={EventShowContainer} />
     </Switch>
--- frontend/src/components/splash/splash.jsx
+++ frontend/src/components/splash/splash.jsx
@@ -1,19 +1,79 @@
 import React from 'react';
-import EventsIndexContainer from "../events/events_index_container";
-
 
 class Splash extends React.Component {
-  constructor(props) {
-    super(props)
+  // constructor(props) {
+  //   super(props)
+  // }
+
+  componentDidMount() {
+    this.props.fetchEvents()
+    this.props.fetchArtists()
   }
 
-  
+  eventCarousel(events) {
+
+  }
+
+  shuffle(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+  }
+
+  getLiveStreams() {
+    let now = (new Date()).getTime()
+
+    let liveStreams = this.props.events.filter(event => {
+      const date = (new Date(event.date)).getTime();
+      return (date < now && date > (now-3600000))
+      // maybe refactor if streaming gives us more info about live streams
+    })
+
+    return this.eventCarousel(this.shuffle(liveStreams))
+  }
+
+  getUpcomingStreams() {
+    let now = (new Date()).getTime()
+
+    let soonStreams = this.props.events.filter(event => {
+      const date = (new Date(event.date)).getTime();
+      return (date > now && date < (now + 86400000))
+    })
+
+    return this.eventCarousel(this.shuffle(soonStreams))
+  }
+
+  getTrendingArtists() {
+
+  }
 
   render() {
+    const lives = this.getLiveStreams()
+    const soons = this.getUpcomingStreams()
+    const randos = this.getTrendingArtists()
 
-    const LiveNow = null
-    const StreamingSoon = null
-    const TrendingArtists = null
+    const LiveNow = lives && lives[0] ? (
+      <div id="live-now">
+        <h3>LIVE</h3>
+        {lives}
+      </div>
+    ) : null;
+
+    const StreamingSoon = soons && soons[0] ? (
+      <div id="streaming-soon">
+        <h3>Streaming Soon...</h3>
+        {soons}
+      </div>
+    ) : null;
+
+    const TrendingArtists = randos && randos[0] ? (
+      <div>
+        <h3>Trending Artists</h3>
+        {randos}
+      </div>
+    ): null;
 
     return(
       <div className='splash'>
@@ -27,20 +87,11 @@ class Splash extends React.Component {
         </div>
         <div className="splash-body">
           <div className="event-category-container">
-            <div id="live-now">
-              <h3>LIVE</h3>
-              {LiveNow}
-            </div>
-            <div id="streaming-soon">
-              <h3>Streaming Soon...</h3>
-              {StreamingSoon}
-            </div>
+            {LiveNow}
+            {StreamingSoon}
           </div>
           <div className="random-artist-container">
-            <h3>Trending Artists</h3>
-            <div>
-              {TrendingArtists}
-            </div>
+            {TrendingArtists}
           </div>
         </div>
       </div>
