e6ca93d9a5a3529816d417ee42858ca910e6b1c2
tjmccabe <tjmccabe3@gmail.com>
2020-05-12 17:39:24 -0700

    Change keys in preparation for Heroku push

---
+++ config/keys_prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  secretOrKey: process.env.SECRET_OR_KEY,
+  awsAccessKeyId: process.env.AWS_ACCESS_KEY,
+  awsSecretAccessKey: process.env.AWS_SECRET_KEY
+}
