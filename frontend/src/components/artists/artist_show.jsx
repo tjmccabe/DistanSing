3675287780aca98f0b9caed7e31ac258b1707b71
Danny <danny.r.huang@gmail.com>
2020-05-12 18:21:50 -0700

    add html for first draft artist show page

--- frontend/src/components/artists/artist_show.jsx
+++ frontend/src/components/artists/artist_show.jsx
@@ -12,13 +12,20 @@ class ArtistShow extends React.Component{
 
   render(){
     const {artist} = this.props
+    const temp_text =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
     if (artist === null || artist === undefined || artist === false) return null;
     return (
       <div>
-        <h1>{artist.artistname}</h1>
-        <div>
-          <p>Hello, I am danny</p>
-          <p>{artist.bio}</p>
+        <div className='artist-show-container'>
+          <img src={`${artist.imageurl}`} alt=""/>
+          <div className='artist-bio'>
+            <h1>{artist.artistname}</h1>
+            <p>{temp_text}</p>
+          </div>
+        </div>
+        <div className='artist-event-index-container'>
+          <h1>{artist.artistname}'s upcoming events:</h1>
         </div>
       </div>
     );
