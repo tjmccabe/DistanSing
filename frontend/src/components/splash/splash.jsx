aef638045d0b7c25649b77c1f3b5b76bb99721d6
tjmccabe <tjmccabe3@gmail.com>
2020-05-12 22:34:51 -0700

    Begin html skeleton of splash

--- frontend/src/components/splash/splash.jsx
+++ frontend/src/components/splash/splash.jsx
@@ -10,11 +10,38 @@ class Splash extends React.Component {
   
 
   render() {
+
+    const LiveNow = null
+    const StreamingSoon = null
+    const TrendingArtists = null
+
     return(
       <div className='splash'>
-        <div>
-          Welcome To DistanSing
-          <EventsIndexContainer />
+        <div className="splash-header">
+          <h2 className="site-heading">
+            Welcome To DistanSing, where we're all only 6 beats apart
+          </h2>
+          <h2 className="site-heading">
+            Check out what's happening!
+          </h2>
+        </div>
+        <div className="splash-body">
+          <div className="event-category-container">
+            <div id="live-now">
+              <h3>LIVE</h3>
+              {LiveNow}
+            </div>
+            <div id="streaming-soon">
+              <h3>Streaming Soon...</h3>
+              {StreamingSoon}
+            </div>
+          </div>
+          <div className="random-artist-container">
+            <h3>Trending Artists</h3>
+            <div>
+              {TrendingArtists}
+            </div>
+          </div>
         </div>
       </div>
     )
