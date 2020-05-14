13d655753f4a747dc6b4e2ec59ac6ffeed603fbb
Glen Park <glenpark00@gmail.com>
2020-05-14 11:08:16 -0700

    Create rudimentary video stream calling

--- frontend/src/components/streams/stream_show.jsx
+++ frontend/src/components/streams/stream_show.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import io from 'socket.io-client';
+import Peer from 'peerjs';
 
 class StreamShow extends React.Component {
   constructor(props) {
@@ -8,7 +9,12 @@ class StreamShow extends React.Component {
       playing: false,
       video: ''
     };
+    // Local variables
     this.socket = io();
+    this.conn = "";
+    this.peerId = "";
+    
+    // Bound functions
     this.startPlaying = this.startPlaying.bind(this);
     this.getMedia = this.getMedia.bind(this);
     this.recStream = this.recStream.bind(this);
@@ -17,42 +23,114 @@ class StreamShow extends React.Component {
   getMedia(callbacks) {
     navigator.getUserMedia =
       navigator.getUserMedia ||
-      navigator.mozGetUserMedia ||
+      navigator.mozGetUserMedia || 
+      navigator.mediaDevices.getUserMedia ||
       navigator.webkitGetUserMedia;
-    const constraints = { audio: true, video: true };
+    const constraints = { audio: true, video: true};
     navigator.getUserMedia(constraints, callbacks.success, callbacks.error);
   }
 
   recStream(stream, elementId) {
     const video = document.getElementById(elementId);
     video.srcObject = stream;
-    video.play();
+    // video.play();
     window.peerStream = stream;
 
   }
 
   startPlaying() {
-    this.setState({ playing: true });
+    this.setState({ playing: true }, () => {
+      this.getMedia({
+        success: (stream) => {
+          window.localstream = stream;
+          this.recStream(stream, "lVideo");
+        },
+        error: (err) => {
+          alert("cannot access your camera");
+          console.log(err);
+        },
+      });
+      // emit
+      
+      // const peer = new Peer({ key: '1wjd5qra8257b9', host: "https://0.peerjs.com" })
+      // const peer = new Peer({ key: '1wjd5qra8257b9' })
+      // const peer = new Peer({ host: "https://0.peerjs.com" })
+      const peer = new Peer()
+  
+      peer.on("open", () => {
+        document.getElementById("displayId").innerHTML = peer.id
+      })
+  
+      peer.on("connection", connection => {
+        this.conn = connection;
+        this.peerId = connection.peer
+        debugger;
 
-    this.getMedia({
-      success: (stream) => {
-        window.localstream = stream;
-        this.recStream(stream, "lVideo");
-      },
-      error: (err) => {
-        alert("cannot access your camera");
+        document.getElementById("connId").value = this.peerId;
+      })
+  
+      peer.on("error", err => {
+        alert(`An error has occurred: ${err}`);
         console.log(err);
-      },
+      })
+  
+      document.getElementById("conn_button").addEventListener("click", () => {
+        let peerId = document.getElementById("connId").value;
+  
+        if (peerId) {
+          this.conn = peer.connect(peerId)
+        } else {
+          alert("Enter an ID");
+          return false;
+        }
+      })
+  
+      peer.on("call", call => {
+        let acceptCall = window.confirm("Do you want to answer this call");
+  
+        if (acceptCall) {
+          call.answer(window.localstream)
+  
+          call.on("stream", stream => {
+            window.peerStream = stream
+  
+            this.recStream(stream, "rVideo")
+          })
+        } else {
+          console.log("call denied");
+        }
+      })
+  
+  
+      document.getElementById("call_button").addEventListener("click", () => {
+        console.log("calling a peer" + this.peerId)
+        console.log(peer)
+  
+        let call = peer.call(this.peerId, window.localstream)
+  
+        call.on("stream", stream => {
+          window.peerStream = stream
+  
+          this.recStream(stream, "rVideo")
+        })
+      })
+
     });
-    // emit
+
+
   }
 
   render() {
     const { video } = this.state;
     return this.state.playing ? (
       <div>
-        <video id="lVideo" src={video}></video>
+        <video id="lVideo" src={video} controls muted autoPlay={true} ></video>
+        <video id="rVideo" src={video} controls muted autoPlay={true} ></video>
+        <p id="displayId"></p>
         <p>Glen's bedroom is poppin!!!</p>
+        <input type="text" id="connId" />
+        <button id="conn_button">Connect</button>
+        <button id="call_button">Call</button>
       </div>
     ) : (
       <button onClick={this.startPlaying}>LET'S FUCKING GET IT</button>
