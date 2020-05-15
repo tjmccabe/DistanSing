44e28617448740e3d391e49bb5d1b2f96ca4f51e
Danny <danny.r.huang@gmail.com>
2020-05-15 09:03:36 -0700

    link artist and event seeds

--- seed.js
+++ seed.js
@@ -16,24 +16,25 @@ function seed() {
   
   
   console.log('please seed')
-  for (let i = 0; i < 5; i++) {
-    let username = faker.internet.userName();
-    let email = faker.internet.email();
-    let password = faker.internet.password();
-    const user = new User({
-      username: `${username}`,
-      email: `${email}`,
-      password: `$${password}`,
-    });
-    console.log(user)
-    // User.collection.insertOne(user);
-  }
-
-  for (let i = 0; i < 5; i++) {
-    let solo = faker.name.findName();
+  // for (let i = 0; i < 20; i++) {
+  //   let username = faker.internet.userName();
+  //   let email = faker.internet.email();
+  //   let password = faker.internet.password();
+  //   const user = new User({
+  //     username: `${username}`,
+  //     email: `${email}`,
+  //     password: `$${password}`,
+  //   });
+  //   console.log(user)
+  //   User.collection.insertOne(user);
+  // }
     let group = faker.name.title();
-    // let artistname = `${solo}`;
-    let artistname = `The ${group}s`;
+    // let artistname = `The ${group}s`;
+    let solo = faker.name.findName();
+    let artistname = `${solo}`;
+  for (let i = 0; i < 1; i++) {
+    // let solo = faker.name.findName();
+    // let artistname = `The ${group}s`;
     let email = faker.internet.email();
     let password = faker.internet.password();
     let bio = faker.lorem.paragraph();
@@ -48,19 +49,16 @@ function seed() {
     });
     console.log(artist);
     artists.push(artist)
-    // Artist.collection.insertOne(artist)
+    Artist.collection.insertOne(artist)
   }
-
-  for (let i = 0; i < 5; i++) {
+   
+  for (let i = 0; i < 1; i++) {
     // Adjectives
     let adj = faker.commerce.productAdjective();
     let group = faker.name.title();
-    let artistname = `The ${group}s`;
-    // let solo= faker.name.findName();
-    // let artistname = `${solo}`;
-    // let name = `See ${artistname} LIVE!`;
+    let name = `See ${artistname} LIVE!`;
     // let name = `${artistname} 2020 Virtual Tour!`;
-    let name = `Join ${solo} at the ${adj} Palooza `
+    // let name = `Join ${solo} at the ${adj} Palooza `
     let description = faker.lorem.paragraph();
     let date = faker.date.between("2020-05-15", "2020-05-16");
     let artist = _.sample(artists)._id;
@@ -81,7 +79,7 @@ function seed() {
       imageurl: imageurl
     });
     console.log(event)
-    // Event.collection.insertOne(event)
+    Event.collection.insertOne(event)
   }
   console.log('finished seeding')
 }
