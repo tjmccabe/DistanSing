214210de6e0b7cf5033d893d9b970df884f9cb7c
darrickyong <darrickyong@gmail.com>
2020-05-15 01:18:32 -0700

    Add user ability to purchase, event/artist links

--- frontend/src/components/artists/artist_show.jsx
+++ frontend/src/components/artists/artist_show.jsx
@@ -1,50 +1,43 @@
-import React from 'react';
-import ShowEventItem from './show_event_item';
-import { Link } from 'react-router-dom';
-
-
-class ArtistShow extends React.Component{
+import React from "react";
+import ShowEventItem from "./show_event_item";
+import { Link } from "react-router-dom";
+class ArtistShow extends React.Component {
   // constructor(props){
   //   super(props)
   // }
-
-
-  componentDidMount(){
-    this.props.fetchArtist(this.props.match.params.id)
+  componentDidMount() {
+    this.props.fetchArtist(this.props.match.params.id);
   }
-
-  render(){
-    const {artist} = this.props
-    if (artist === null || artist === undefined || artist === false) return null;
-    console.log(artist)
+  render() {
+    const { artist } = this.props;
+    if (!artist) return null;
+    console.log(artist);
     const temp_text =
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-    
-    
-    
-      return (
-        <div className='artist-show-container'>
-          <div className="artist-bio-container">
-            <img src={`${artist.imageurl}`} alt="" />
-            <div className="artist-bio">
-              <h1>{artist.artistname}</h1>
-              <p className="artist-bio-text">{temp_text}</p>
-            </div>
-          </div>
-          <Link to={`/artists/${this.props.match.params.id}/edit`}>Edit</Link>
-          <div className="artist-event-index-container">
-            <h1>{artist.artistname}'s upcoming events:</h1>
-            <div className='show-item-container'>
-              {Object.values(artist.artistEvents).map((event) => (
-                <ShowEventItem key={event._id} event={event} />
-              ))}
-            </div>
+    const ArtistEvents =
+      artist.artistEvents && Object.values(artist.artistEvents)[0] ? (
+        <div className="artist-event-index-container">
+          <h1>{artist.artistname}'s upcoming events:</h1>
+          <div className="show-item-container">
+            {Object.values(artist.artistEvents).map((event) => (
+              <ShowEventItem key={event._id} event={event} />
+            ))}
           </div>
         </div>
-      );
+      ) : null;
+    return (
+      <div className="artist-show-container">
+        <div className="artist-bio-container">
+          <img src={`${artist.imageurl}`} alt="" />
+          <div className="artist-bio">
+            <h1>{artist.artistname}</h1>
+            <p className="artist-bio-text">{temp_text}</p>
+          </div>
+        </div>
+        <Link to={`/artists/${this.props.match.params.id}/edit`}>Edit</Link>
+        {ArtistEvents}
+      </div>
+    );
   }
-
-
 }
-
 export default ArtistShow;
--- frontend/src/components/artists/show_event_item.jsx
+++ frontend/src/components/artists/show_event_item.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { withRouter } from "react-router-dom";
 
 class ShowEventItem extends React.Component {
-  
+    
 
   render(){
     const {event} = this.props
     console.log(event)
     return (
-      <div className="show-item">
+      <div className="show-item" onClick={() => this.props.history.push(`/events/${event._id}`)}>
         <figure></figure>
         <div className='item-details'>
           <div className="item-text">
@@ -21,4 +22,4 @@ class ShowEventItem extends React.Component {
   }
 }
 
-export default ShowEventItem;
+export default withRouter(ShowEventItem);
