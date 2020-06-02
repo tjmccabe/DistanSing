854a9dc3a754d3693b164a90b6b83c22794110c5
tjmccabe <tjmccabe3@gmail.com>
2020-06-02 14:10:24 -0700

    Add background to search

--- frontend/src/components/search/search_index.jsx
+++ frontend/src/components/search/search_index.jsx
@@ -34,42 +34,51 @@ class SearchIndex extends React.Component {
     if (!searchResults) return null;
     return (
       <div className="search-results-container">
-        <h1 className="search-results-header">All results for: <span className="search-fragment">"{match.params.fragment}"</span></h1>
-        <h2>Artists</h2>
-        { searchResults.artists.length > 0 ? 
-          <div className="search-artists-results">
-            { searchResults.artists.map(artist => 
-            <div 
-              key={artist._id}
-              className="artist-feature-item artist-search-item"
-              style={{
-                backgroundImage: `url(${artist.imageurl})`,
-                width: '200px',
-                height: '200px'
-              }}
-              onClick={() => history.push(`/artists/${artist._id}`)}
-              >
-                <div className="artist-feature-item-filter">
-                  <div>{artist.artistname}</div>
-                  <div>{artist.genre === "None" ? null : artist.genre}</div>
+        <div
+          className="background-search"
+          style={{
+            backgroundImage: `url("https://images.unsplash.com/photo-1516280440614-37939bbacd81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80")`,
+          }}
+        >
+          <div className="background-search-filter">
+            <h1 className="search-results-header">All results for: <span className="search-fragment">"{match.params.fragment}"</span></h1>
+            <h2>Artists</h2>
+            { searchResults.artists.length > 0 ? 
+              <div className="search-artists-results">
+                { searchResults.artists.map(artist => 
+                <div 
+                  key={artist._id}
+                  className="artist-feature-item artist-search-item"
+                  style={{
+                    backgroundImage: `url(${artist.imageurl})`,
+                    width: '200px',
+                    height: '200px'
+                  }}
+                  onClick={() => history.push(`/artists/${artist._id}`)}
+                  >
+                    <div className="artist-feature-item-filter">
+                      <div>{artist.artistname}</div>
+                      <div>{artist.genre === "None" ? null : artist.genre}</div>
+                    </div>
                 </div>
-            </div>
-            )}
+                )}
+              </div>
+              : <div className="search-artists-results">No artists found</div>
+              }
+            <h2>Events</h2>
+            { searchResults.events.length > 0 ?
+              <div className="search-events-results">
+                {searchResults.events.map(event => 
+                  <ShowEventItem
+                    key={event._id}
+                    event={event}
+                  />
+                )}
+              </div>
+              : <div className="search-events-results">No events found</div>
+            }
           </div>
-          : <div className="search-artists-results">No artists found</div>
-          }
-        <h2>Events</h2>
-        { searchResults.events.length > 0 ?
-          <div className="search-events-results">
-            {searchResults.events.map(event => 
-              <ShowEventItem
-                key={event._id}
-                event={event}
-              />
-            )}
-          </div>
-          : <div className="search-events-results">No events found</div>
-        }
+        </div>
       </div>
     )
   } 
