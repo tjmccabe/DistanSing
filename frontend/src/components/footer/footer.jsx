a139a6e987c96ea805dd96b96f46af4ec20a2346
Danny <danny.r.huang@gmail.com>
2020-06-01 21:58:38 -0700

    style footer

--- frontend/src/components/footer/footer.jsx
+++ frontend/src/components/footer/footer.jsx
@@ -21,34 +21,46 @@ const Footer = () => {
   }
   return (
     <div className="footer">
-      <div className="footer-about">
-        {Object.keys(us).map((engineer, idx) => 
-          <div key={idx} className="footer-item-container">
-            <img className="foot-pic" src="https://distansing-dev.s3-us-west-1.amazonaws.com/tj.png" alt="" />
-            <div className="footer-item-content">
-              <div>{engineer}</div>
-              <div className="footer-icons">
-                <a 
-                  href={us[engineer].github}><img className="footer-git-logo" src="https://distansing-dev.s3-us-west-1.amazonaws.com/git_og.png" alt=""/>
-                </a>
-                <a 
-                  href={us[engineer].linkedin}>
-                  <img className="footer-li-logo" src="https://distansing-dev.s3-us-west-1.amazonaws.com/linked_in_icon.png" alt="" />
-                </a>
-              </div>
-            </div>
-          </div>
-        )}
-      </div>
-      <div className="footer-site-info">
+      {/* <div className="footer-about"></div> */}
+      {/* <div className="footer-site-info"> */}
         <div className="footer-site-info-left">
           <div className="footer-info-logo">DistanSing</div>
-          <div className="footer-info-text">By using this site, you agree to our Terms of Service (the only term being please do not hack us!)</div>
+          {Object.keys(us).map((engineer, idx) => (
+            <div key={idx} className="footer-item-container">
+              <img
+                className="foot-pic"
+                src="https://distansing-dev.s3-us-west-1.amazonaws.com/tj.png"
+                alt=""
+              />
+              <div className="footer-item-content">
+                <div>{engineer}</div>
+                <div className="footer-icons">
+                  <a href={us[engineer].github}>
+                    <img
+                      className="footer-git-logo"
+                      src="https://distansing-dev.s3-us-west-1.amazonaws.com/git_og.png"
+                      alt=""
+                    />
+                  </a>
+                  <a href={us[engineer].linkedin}>
+                    <img
+                      className="footer-li-logo"
+                      src="https://distansing-dev.s3-us-west-1.amazonaws.com/linked_in_icon.png"
+                      alt=""
+                    />
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))}
+
+        {/* </div> */}
+        <div className="footer-info-copyright">
+          &#169; Kunsole Lawg Studios 2020
         </div>
-        <div className="footer-info-copyright">&#169; Kunsole Lawg Studios 2020</div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Footer;
