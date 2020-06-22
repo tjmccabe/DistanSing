ab82091763d7564ec46d2398e8d98480a705fc88
Danny <danny.r.huang@gmail.com>
2020-06-22 11:23:51 -0700

    fix seed and trending artists

--- seed.js
+++ seed.js
@@ -48,14 +48,13 @@ function seed() {
   //   artists.push(artist)
   //   Artist.collection.insertOne(artist)
   // }
+  const event_date = new Date
+  event_date.setDate( event_date.getDate() + 7)
   let CXCW = new Event({
     name: "Couch By Couchwest Festival",
     description:
       "Couch by Couchwest is an annual conglomeration of social distancing, human welfare, and music festivals organized jointly by DistanSing Corporation. It launched in 2020 in order to showcase the frontend skills of its organizers.",
-    date: faker.date.between(
-      `${year}-0${month}-${day}`,
-      `${year}-0${month}-${day + 1}`
-    ),
+    date: event_date,
     artist: "5ed72c0810435f4933a2a981",
     price: 30,
     imageurl: "https://distansing-dev.s3-us-west-1.amazonaws.com/SXSW.png",
@@ -64,10 +63,7 @@ function seed() {
     name: "Electric DistanSing Carnival",
     description:
       "Electric DistanSing Carnival, commonly known as EDC, is the largest virtual electronic dance music festival in the world. The annual flagship event is now held weekly, put on by The Demo Artists.",
-    date: faker.date.between(
-      `${year}-0${month}-${day}`,
-      `${year}-0${month}-${day + 1}`
-    ),
+    date: event_date,
     artist: "5ed7247de51f52f959016c5e",
     price: 50,
     imageurl:
@@ -77,10 +73,7 @@ function seed() {
     name: "Quaranchella",
     description:
       "The Quaranchella Valley Music and Arts Festival is a weekly music and arts festival held on the world's greatest virtual concert site based out of San Francisco, California. It was co-founded by Danny Huang, Glen Park, Darrick Yong, and TJ McCabe and has stood a monument to their humble beginnings ever since.",
-    date: faker.date.between(
-      `${year}-0${month}-${day}`,
-      `${year}-0${month}-${day + 1}`
-    ),
+    date: event_date,
     artist: "5ed72c0810435f4933a2a981",
     price: 40,
     imageurl:
@@ -90,10 +83,7 @@ function seed() {
     name: "CageCoach Festival",
     description:
       "The Cagecoach Festival is one of the biggest country music festivals in the world, taking place weekly on the DistanSing webapp. It is both a celebration of country music and software engineering skills.",
-    date: faker.date.between(
-      `${year}-0${month}-${day}`,
-      `${year}-0${month}-${day + 1}`
-    ),
+    date: event_date,
     artist: "5ed7247de51f52f959016c5e",
     price: 35,
     imageurl: "https://distansing-dev.s3-us-west-1.amazonaws.com/Cagecoach.png",
@@ -102,23 +92,18 @@ function seed() {
     name: "Electric Zoom Festival",
     description:
       "Electric Zoom is a weekly electronic music festival held virtually on the DistanSing webapp. The festival represents all genres of electronic music, bringing top international DJs and live acts from multiple countries to four stages.",
-    date: faker.date.between(
-      `${year}-0${month}-${day}`,
-      `${year}-0${month}-${day + 1}`
-    ),
+    date: event_date,
     artist: "5ed7247de51f52f959016c5e",
     price: 45,
     imageurl:
       "https://distansing-dev.s3-us-west-1.amazonaws.com/electriczoom.png",
   });
   let splash_seeds = [CXCW, EZ, CageCoach, EDC, Quaranchella]
+  // console.log(splash_seeds)
   Event.collection.insert(splash_seeds)
 
 
-  const current_date = new Date
-  const month = current_date.getMonth() + 1
-  const day = current_date.getDate() + 7
-  const year = current_date.getFullYear()
+  
   const artists = 
     [["5ed72c0810435f4933a2a981", "DJ Sanitizee", "https://distansing-dev.s3.us-west-1.amazonaws.com/dj_sanitizzy-1591162884752.jpg"], 
       ["5ed7247de51f52f959016c5e", "The Demo Artists", "https://distansing-dev.s3-us-west-1.amazonaws.com/default_artist_image_mask_on.jpg"], 
@@ -136,10 +121,15 @@ function seed() {
     let adj = _.sample(adjectives);
     let event_name = _.sample(events);
     let name = `Join ${artistname} at the ${adj} ${event_name}`
-    let date = current_date;
-    const month_name = date.toString().slice(4, 7);
+    let newDate = new Date()
+    let number = Math.floor(Math.random() * Math.floor(13))
+    newDate.setDate( newDate.getDate() + 7 + number)
 
-    let description = `Check out ${artistname} streaming to you live on ${month_name} ${day}th. Get your ticket now and come back to the event page at start time to tune in live!`
+    // console.log(number)
+    // console.log(typeof Math.floor(Math.random() * Math.floor(13)))
+    const month_name = newDate.toString().slice(4, 7);
+
+    let description = `Check out ${artistname} streaming to you live on ${month_name} ${newDate.getDate()}. Get your ticket now and come back to the event page at start time to tune in live!`
     let price = faker.random.number({
       min: 10,
       max: 50,
@@ -149,15 +139,12 @@ function seed() {
     let event = new Event({
       name: `${name}`,
       description: `${description}`,
-      date: faker.date.between(
-        `${year}-0${month}-${day}`,
-        `${year}-0${month}-${day + 1}`
-      ),
+      date: newDate,
       artist: artistId,
       price: price,
       imageurl: artists[i][2]
     });
-    console.log(event)
+    // console.log(event)
     Event.collection.insertOne(event)
   }
 
